0717ffd20e28af9dde44dd2d999ae68f4435b19c
itou-daiki <141300506+itou-daiki@users.noreply.github.com>
2023-09-11 11:55:09 +0000

    修正

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 import librosa
 import librosa.display
 import pandas as pd
+import os
+import soundfile as sf
+
 
 # Streamlitのページ設定
 st.set_page_config(page_title="音声解析Webアプリ", layout="wide")
@@ -14,7 +17,15 @@ st.caption("Created by Daiki Ito")
 st.subheader("音声の多角的な可視化と特徴点の算出")
 
 # 音声ファイルのアップロード
-uploaded_file = st.file_uploader("音声ファイルをアップロードしてください", type=["wav", "mp3","m4a"])
+uploaded_file = st.file_uploader
+if uploaded_file:
+    # Convert m4a to wav if needed
+    if uploaded_file.name.endswith('.m4a'):
+        y, sr = librosa.load(uploaded_file, sr=None)
+        temp_wav = "temp_converted.wav"
+        sf.write(temp_wav, y, sr)
+        uploaded_file = open(temp_wav, 'rb')
+("音声ファイルをアップロードしてください", type=["wav", "mp3","m4a"])
 
 # 音声の特徴点をテーブル形式で表示
 def display_features(df, title, description):
